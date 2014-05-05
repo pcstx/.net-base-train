76810df4f54fd84170cbd14531b2ebcea41a9240
pcstx <pcstx@163.com>
2014-05-05 23:45:37 +0800

    值类型目

--- demoCode/demoCode/demoCode/Program.cs
+++ demoCode/demoCode/demoCode/Program.cs
@@ -9,6 +9,27 @@ namespace demoCode
     {
         static void Main(string[] args)
         {
+            ValPoint vPoint;
+            vPoint.x = 1;
+            vPoint.y = 2;
+            Console.WriteLine(vPoint.y);          
+
+            ValPoint vPoint1 = new ValPoint();
+            Console.WriteLine(vPoint1.x);
+
+            Console.ReadLine();
         }
     }
+    public struct ValPoint 
+    {
+        public int x;
+        public int y;
+
+        public ValPoint(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        } 
+    }
+
 }
